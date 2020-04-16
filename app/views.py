342cdce432595cb1960ba41bae532c76952a0e17
AntoineMaurin <antoine.maurin26@gmail.com>
2020-04-16 18:22:45 +0200

    Test avec variables d'environnement

--- app/views.py
+++ app/views.py
@@ -1,10 +1,17 @@
+from dotenv import load_dotenv
+import os
+
 from flask import render_template, jsonify, request
 
 from . import app
 from . import utils
 
+load_dotenv()
+
 @app.route("/")
 def home():
+    SECRET_VAR = os.getenv("MAVARIABLE")
+    print('MAVARIABLE : ', SECRET_VAR)
     return render_template("index.html")
 
 @app.route("/ajax", methods=["POST"])
